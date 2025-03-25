1894d7ca4281fd6c63d33a05280e828f6d2d9d02
gdim47 <gdim47@protonmail.com>
2025-03-26 01:48:59 +0200

    ci: feat: add wine builtin tag to generated dlls for vk_lib jobs

--- create_vk_lib_archive.sh
+++ create_vk_lib_archive.sh
@@ -10,10 +10,12 @@ export VK_LIB_REF="${VK_LIB_REF:-$(git rev-parse HEAD)}"
 export VK_LIB_SRC_DIR="${VK_LIB_SRC_DIR:-/workdir/${VK_LIB_NAME}}"
 export VK_LIB_BUILD_DIR="${VK_LIB_BUILD_DIR:-/workdir/${VK_LIB_NAME}-build}"
 export VK_LIB_PREFIX_DIR="${VK_LIB_PREFIX_DIR:-/workdir/prefix-${VK_LIB_NAME}-${VK_LIB_TAG}-${VK_LIB_REF}}"
+export VK_LIB_LIB_DIR="${VK_LIB_PREFIX_DIR}/lib/wine/aarch64-windows"
 
 export CONFIG_OPTIONS="
     --cross-file /toolchains/arm64ec-w64-mingw32.txt \
     --buildtype debugoptimized \
+    --bindir lib/wine/aarch64-windows \
 "
 
 # hack: add lib src dir to git's safe.directory
@@ -28,5 +30,12 @@ cd "${VK_LIB_BUILD_DIR}/build-arm64ec"
 echo "Building ${VK_LIB_NAME} arm64ec"
 ninja -j$(nproc)
 ninja install
+
+echo "Add wine builtin tag to generated dlls"
+for f in "${VK_LIB_LIB_DIR}"/*.dll; do
+    if [ -f "$f" ] && [ ! -L "$f" ]; then
+        dd bs=32 count=1 seek=2 conv=notrunc if=/toolchains/wine_builtin.bin of="$f"
+    fi
+done
 
 echo "${VK_LIB_NAME} build finished"
